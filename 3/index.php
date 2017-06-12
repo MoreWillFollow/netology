5cdfcf6436e0f2bd2826a1065fcc32328185e652
MoreWillFollow <anarnebiyev@gmail.com>
2017-06-12 19:56:07 +0400

    Доработал домашнее задание 1_3

--- 3/index.php
+++ 3/index.php
@@ -1,3 +1,88 @@
+
+
+<?php
+error_reporting(E_ALL);
+
+$data = [
+    "Africa" => [
+        "Canis lupus",
+        "Ovis orientalis",
+        "Sus scrofa ololoshka",
+        "Capra aegagrus",
+        "Bos primigenius",
+        "Felis silvestris lybica"
+    ],
+    "Europa" => [
+        "Gallus gallus",
+        "Bond Cavia tschudii",
+        "Equus asinus",
+        "Anas platyrhynchos"
+    ],
+    "Asia" => [
+        "Obi-Wan Bubalus arnee",
+        "Equus ferus",
+        "777777777777777 Camelus dromedarius",
+        "Bombyx mandarina",
+        "Columba livia",
+        "Anser anser",
+        "Bos grunniens"
+    ]
+];
+
+//Находим и вырезаем ПЕРВОЕ слово + забиваем в массив $first_word
+//Находим и вырезаем ВТОРОЕ слово + забиваем в массив $second_word
+
+foreach ($data as $continent => $animals) {
+    foreach ($animals as $key => $value) {
+
+        if (substr_count($value,  " ") === 1) {
+
+            $found = substr($value, 0,(strpos($value, " ")));
+            $first_words["$continent"][] = $found;
+
+            $start = strpos($value, " ");
+            $start1 = $start + 1;
+            $end_s = strpos($value, " ", $start1);
+            if (!$end_s > 0) {
+                $found = substr($value, $start);
+
+            }
+            else {
+                $length = $end_s-$start;
+                $found = substr($value, $start, $length);
+            }
+            $second_words[] = $found;
+
+
+        }
+
+    };
+};
+
+// ПЕРЕМЕШИВАЕМ ОБА МАССИВА
+foreach ($first_words as $key => $value) {
+    shuffle($first_words[$key]);
+
+}
+shuffle ($second_words);
+
+// СКЛЕИВАЮ ФРАГМЕНТЫ В ОДИН МАССИВ И ВЫВОЖУ НОВЫЕ НАЗВАНИЯ
+$j = 0;
+$new_array = [];
+
+foreach ($first_words as $continent => $first_animals) {
+    echo "<h2>".$continent."</h2>";
+
+    foreach ($first_animals as $key => $animal) {
+        echo $animal." ".$second_words[$j]."<br>";
+        $j++;
+    }
+}
+
+?>
+
+
+
 <html>
 <head>
     <title>Домашнее задание #3</title>
@@ -21,93 +106,3 @@
     }
 </style>
 </html>
-
-<?php
-error_reporting(E_ALL);
-
-$data = [
-    "Africa" => [
-        "Canis lupus",
-        "Ovis orientalis",
-        "Sus scrofa",
-        "Capra aegagrus",
-        "Bos primigenius",
-        "Felis silvestris lybica"
-    ],
-    "Europa" => [
-        "Gallus gallus",
-        "Cavia tschudii",
-        "Equus asinus",
-        "Anas platyrhynchos"
-    ],
-    "Asia" => [
-        "Bubalus arnee",
-        "Equus ferus",
-        "Camelus dromedarius",
-        "Bombyx mandarina",
-        "Columba livia",
-        "Anser anser",
-        "Bos grunniens"
-    ]
-];
-
-//Находим и вырезаем ПЕРВОЕ слово + забиваем в массив $first_word
-foreach ($data as $continent => $animals) {
-    foreach ($animals as $key => $value) {
-        $found = substr($value, 0,(strpos($value, " ")));
-        $first_words[$continent][] = $found
-        ;
-    };
-};
-
-//Находим и вырезаем ВТОРОЕ слово + забиваем в массив $second_word
-
-foreach ($data as $continent => $animal) {
-    foreach ($animal as $key => $value) {
-        $start = strpos($value, " ");
-        $start1 = $start + 1;
-        $end_s = strpos($value, " ", $start1);
-        if (!$end_s > 0) {
-            $found = substr($value, $start);
-
-        }
-        else {
-            $length = $end_s-$start;
-            $found = substr($value, $start, $length);
-        }
-        $second_words[] = $found;
-    }
-}
-shuffle ($second_words);
-$count_elements = count($first_words);
-
-
-// СКЛЕИВАЮ ФРАГМЕНТЫ В ОДИН МАССИВ И ВЫВОЖУ НОВЫЕ НАЗВАНИЯ
-
-$i = 0;
-$c = 0;
-$j = 0;
-while ( $c < $count_elements) {
-    foreach ($first_words as $key => $value) {
-        echo "<h2>Континент: ".$key."</h2>";
-
-        echo "<br><pre>";
-        var_dump($value);
-        echo "</pre><hr>";
-
-        foreach ($value as $continent => $animal_name) {
-            $counter = count($first_words[$key]);
-            $new_animals[] = $first_words[$key][$i].$second_words[$i];
-            $i++;
-            echo $new_animals[$j];
-            if ($i == $counter) {$i=0; echo ".";}
-            else {
-                echo ", ";
-            }
-            $j++;
-        }
-
-        $c++;
-    }
-}
-?>
