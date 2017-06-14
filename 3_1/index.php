b29fe895bdb9bb58f095564bc41f2965ab2d7f08
MoreWillFollow <anarnebiyev@gmail.com>
2017-06-15 00:34:07 +0400

    Домашняя работа 3_1. Первая версия.

--- 3_1/index.php
+++ 3_1/index.php
@@ -20,6 +20,7 @@ include "classes/Tv.php";
 
 <section>
     <h2>1. Инкапсуляция - своими словами.</h2>
+    <hr>
     <p>Мы изолируем некие данные и процессы в капсуле. В этой капсуле мы делаем окошко где сидит чудик и принимает определенные данные. И если чудик одобряет входящие данные, то капсула выполняет свою работу. Допустим, возводит числа в квадрат. А если нет, то чудик ругается :)
 
         Суть.
@@ -28,6 +29,7 @@ include "classes/Tv.php";
 
 <section>
     <h2>2. Плюсы и минусы объектов</h2>
+    <hr>
     <p><b>Плюсы:</b></p>
     <p>-Данные и методы надежно защищены и не могут быть случайно изменены из другого места кода вне объекта.</p>
     <p>-Можно обновлять код в капсуле, не рискую нарушить работу сервиса. Главное чтобы входящие и исходящие данные оставались неизменными.
@@ -39,58 +41,64 @@ include "classes/Tv.php";
 
 <section>
     <h2>5 классов и по 2 объекта каждого класса</h2>
-    <p><b>Класс Машина</b></p>
-    <?php
-    show_source("classes/Car.php");
-    ?>
-    <p><b>Несколько объектов</b></p>
-    <?
-    include "objects/car.php";
-    ?>
     <hr>
+    <div class="one">
+        <p><b>Класс Машина</b></p>
+        <?php
+        show_source("classes/Car.php");
+        ?>
+        <p><b>Несколько объектов</b></p>
+        <?
+        include "objects/car.php";
+        ?>
+        <hr>
+    </div>
+    <div class="two">
+        <p><b>Класс Телевизор</b></p>
+        <?php
+        show_source("classes/Tv.php");
+        ?>
+        <p><b>Несколько объектов</b></p>
+        <?
+        include "objects/tv.php";
+        ?>
+        <hr>
+    </div>
 
-    <p><b>Класс Телевизор</b></p>
-    <?php
-    show_source("classes/Tv.php");
-    ?>
-    <p><b>Несколько объектов</b></p>
-    <?
-    include "objects/tv.php";
-    ?>
-    <hr>
+    <div class="one">
+        <p><b>Класс Шариковая ручка</b></p>
+        <?php
+        show_source("classes/Pen.php");
+        ?>
+        <p><b>Несколько объектов</b></p>
+        <?
+        include "objects/pen.php";
+        ?>
+        <hr>
+    </div>
 
+    <div class="two">
+        <p><b>Класс Утка</b></p>
+        <?php
+        show_source("classes/Duck.php");
+        ?>
+        <p><b>Несколько объектов</b></p>
+        <?
+        include "objects/duck.php";
+        ?>
+        <hr>
+    </div>
 
-    <p><b>Класс Шариковая ручка</b></p>
-    <?php
-    show_source("classes/Pen.php");
-    ?>
-    <p><b>Несколько объектов</b></p>
-    <?
-    include "objects/pen.php";
-    ?>
-    <hr>
-
-
-    <p><b>Класс Утка</b></p>
-    <?php
-    show_source("classes/Duck.php");
-    ?>
-    <p><b>Несколько объектов</b></p>
-    <?
-    include "objects/duck.php";
-    ?>
-    <hr>
-
-
-    <p><b>Класс Товар</b></p>
-    <?php
-    show_source("classes/Goods.php");
-    ?>
-    <p><b>Несколько объектов</b></p>
-    <?
-    include "objects/goods.php";
-    ?>
-
+    <div class="one">
+        <p><b>Класс Товар</b></p>
+        <?php
+        show_source("classes/Goods.php");
+        ?>
+        <p><b>Несколько объектов</b></p>
+        <?
+        include "objects/goods.php";
+        ?>
+    </div>
 
 
 </section>
